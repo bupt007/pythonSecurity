c2f78ac85afae876b7bf21942efc640d72210d75
pf <liduzhizi@gmail.com>
2014-03-26 20:31:15 -0700

    xss demo

---
+++ xss.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding:utf-8
+# @author:pengfei filename: xss.py
+
+'''
+	To demonstrate how to dig xss with python code
+'''
+
+from webBrowser import FormInjectionBrowser
+
+
+if __name__=="__main__":
+
+	injectData = ['<script>alert("XSS")</script>',
+				  '<SCRIPT>alert("XSS")</SCRIPT>',
+				  '<ScRiPT>alert("XSS")</script>',
+				  '<script>alert(String.fromCharCode(88,83,83))</script>']
+	expectData = [ item for item in injectData]
+
+	fi = FormInjectionBrowser()
+	fi.inject()
+	fi.close()
+
+
