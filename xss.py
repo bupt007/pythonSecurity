c42614c6bf13101028a10239bb1e0d1d43eada00
bupt007 <bupt007@users.noreply.github.com>
2014-03-27 11:35:50 +0800

    Update xss.py

--- xss.py
+++ xss.py
@@ -12,9 +12,9 @@ from webBrowser import FormInjectionBrowser
 if __name__=="__main__":
 
 	injectData = ['<script>alert("XSS")</script>',
-				  '<SCRIPT>alert("XSS")</SCRIPT>',
-				  '<ScRiPT>alert("XSS")</script>',
-				  '<script>alert(String.fromCharCode(88,83,83))</script>']
+		      '<SCRIPT>alert("XSS")</SCRIPT>',
+		      '<ScRiPT>alert("XSS")</script>',
+		      '<script>alert(String.fromCharCode(88,83,83))</script>']
 	expectData = [ item for item in injectData]
 
 	fi = FormInjectionBrowser()
